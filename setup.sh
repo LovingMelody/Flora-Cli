98289094fb403d85e4f1100362dcd07efaf2cc3d
Fuzen <fuzen@protonmail.com>
2016-07-02 14:29:24 -0400

    Project Start
    Project is at its usable state

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+/usr/bin/echo "Setting Up Flora-Cli"
+#/usr/bin/mkdir /usr/local/bin/Fuzzy
+#/usr/bin/mkdir /usr/local/bin/Fuzzy/Flora-cli/
+/usr/bin/cp /Flora-Cli/main.py /usr/local/bin/flora-cli
+/usr/bin/chmod +x /usr/local/bin/flora-cli
