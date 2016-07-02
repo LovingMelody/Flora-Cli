c658051fc9a18e25787979bf1c981a41cbbb6a38
Kitty <NekoKitty@users.noreply.github.com>
2016-07-02 19:36:22 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
-/usr/bin/echo "Setting Up Flora-Cli"
+echo "Setting Up Flora-Cli"
 #/usr/bin/mkdir /usr/local/bin/Fuzzy
 #/usr/bin/mkdir /usr/local/bin/Fuzzy/Flora-cli/
-/usr/bin/cp /Flora-Cli/main.py /usr/local/bin/flora-cli
-/usr/bin/chmod +x /usr/local/bin/flora-cli
+cp /Flora-Cli/main.py /usr/local/bin/flora-cli
+chmod +x /usr/local/bin/flora-cli
+echo "Done"
+flora-cli
