d839a5b7975b0fa9731f66cc5fabfca23c7f7b8b
Fuzen <fuzen@protonmail.com>
2016-07-02 19:37:13 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 echo "Setting Up Flora-Cli"
 #/usr/bin/mkdir /usr/local/bin/Fuzzy
 #/usr/bin/mkdir /usr/local/bin/Fuzzy/Flora-cli/
-cp /Flora-Cli/main.py /usr/local/bin/flora-cli
+cp Flora-Cli/main.py /usr/local/bin/flora-cli
 chmod +x /usr/local/bin/flora-cli
 echo "Done"
 flora-cli
