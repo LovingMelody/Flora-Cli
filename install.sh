262dc5ab7d0715111c691d69ab3374a7b1c78df4
Fuzen <fuzen@protonmail.com>
2016-07-03 17:46:38 -0400

    Updates
    Added a task manager (pretty basic)

--- install.sh
+++ install.sh
@@ -1,6 +1,2 @@
-#!/bin/bash
-echo 'Installing Flora Cli'
-sudo curl -o /usr/local/bin/flora-cli https://raw.githubusercontent.com/NekoKitty/Flora-Cli/master/Flora-Cli/main.py
-sudo chmod +x /usr/local/bin/flora-cli
-echo 'Done'
-
+#!/usr/bin/env bash
+echo 'Installing Flora Cli' && sudo curl -o /usr/local/bin/flora-cli https://raw.githubusercontent.com/NekoKitty/Flora-Cli/master/Flora-Cli/main.py && sudo chmod +x /usr/local/bin/flora-cli && echo 'Done'
