38ec812b6ad8fcb4622814d15c538997b6d50560
Fuzen <Fuzen-py@users.noreply.github.com>
2016-07-10 14:03:57 -0400

    Delete install.sh

--- install.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-echo 'Installing Flora Cli' && sudo curl -o /usr/local/bin/flora-cli https://raw.githubusercontent.com/NekoKitty/Flora-Cli/master/Flora-Cli/main.py && sudo chmod +x /usr/local/bin/flora-cli && echo 'Done'
