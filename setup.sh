508f2a6a4f300d9480ff94c0ed75538390726287
Fuzen <fuzen@protonmail.com>
2016-07-22 13:45:58 -0400

    Updates o/
    A few updates more to come soon

--- setup.sh
+++ setup.sh
@@ -2,6 +2,4 @@
 echo "Setting Up Flora-Cli"
 #/usr/bin/mkdir /usr/local/bin/Fuzzy
 #/usr/bin/mkdir /usr/local/bin/Fuzzy/Flora-cli/
-cp Flora-Cli/main.py /usr/local/bin/flora-cli
-chmod +x /usr/local/bin/flora-cli
-echo "Done"
+python3 setup.py install
