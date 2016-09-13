c03fa07934b3b1f86abdaabc40764813e51d375a
Fuzen <fuzen@protonmail.com>
2016-09-12 23:49:30 -0400

    Update
    Broken Update

---
+++ FloraCli/__init__.py
@@ -0,0 +1,6 @@
+from .flora_cli import Flora, main
+__title__ = 'Flora-Cli'
+__version__ = '0.0.1'
+__license__ = 'MIT'
+__author__ = 'Fuzen.py'
+__copyright__ = 'Copyright 2016 Fuzen'
--- setup.py
+++ setup.py
@@ -1,66 +1,28 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
-import pkg_resources
-import sys
-from os import name as dist_name, rename
-from os.path import dirname, join, realpath
-import os
-try:
-    if int(pkg_resources.get_distribution("pip").version.split('.')[0]) < 6:
-        print('pip older than 6.0 not supported, please upgrade pip with:\n\n'
-              '    pip install -U pip')
-        sys.exit(-1)
-except pkg_resources.DistributionNotFound:
-    pass
-version = sys.version_info[:2]
-if version < (3, 5):
-    print('Flora-Cli requires Python version 3.5 or later' +
-          ' ({}.{} detected).'.format(*version))
-    sys.exit(-1)
+from setuptools import setup
 
-VERSION = '3.10a'
-if os.name == 'nt':
-    setup(name='Flora-Cli',
-          version=VERSION,
-          description="A basic commandline utility",
-          long_description="A basic commandline utility developed by Fuzen-py",
-          author='Fuzen.py',
-          author_email='',
-          url='https://github.com/Fuzen-py/Flora-Cli',
-          license='MIT',
-          packages=find_packages(exclude=['ez_setup', 'examples', 'tests', 'tests.*', 'release']),
-          include_package_data=True,
-          zip_safe=False,
-          install_requires=['psutil', 'speedtest-cli', 'logbook'],
-          extras_require={},
-          scripts=['flora-cli.py'])
-else:
-    rename(join(dirname(realpath(__file__)), 'flora-cli.py'), join(dirname(realpath(__file__)), 'flora-cli'))
-    setup(name='Flora-Cli',
-          version=VERSION,
-          description="A basic commandline utility",
-          long_description="A basic commandline utility developed by Fuzen-py",
-          author='Fuzen.py',
-          author_email='',
-          url='https://github.com/Fuzen-py/Flora-Cli',
-          license='MIT',
-          packages=find_packages(
-              exclude=[
-                  'ez_setup',
-                  'examples',
-                  'tests',
-                  'tests.*',
-                  'release'
-                  ]
-              ),
-          include_package_data=True,
-          zip_safe=False,
-          install_requires=[
-              'psutil',
-              'speedtest-cli',
-              'logbook'],
-          extras_require={
-              },
-          scripts =['flora-cli']
-          )
-    rename(join(dirname(realpath(__file__)), 'flora-cli'), join(dirname(realpath(__file__)), 'flora-cli.py'))
+setup(
+    name='Flora-Cli',
+    version='1.0.4',
+    packages=['FloraCli'],
+    url='https://github.com/Fuzen-py/Flora-cli',
+    license='MIT',
+    author='Fuzen.py',
+    author_email='',
+    description='Python 3 Command Line',
+    long_description='A Python3 command line utility that is currently not stable',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Linux',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Scripts'
+    ],
+    keywords="Commandline update",
+    install_requires=['psutil', 'logbook', 'speedtest-cli'],
+    entry_points={
+        'console_scripts': [
+            'flora-cli=FloraCli:main'
+        ]
+    })
